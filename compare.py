bfd0da131aad956380441cf145f47179221dda20
Jordan Retterer <jordanr64@gmail.com>
2017-07-22 13:49:31 -0400

    Get the names of two players to compare

--- compare.py
+++ compare.py
@@ -35,10 +35,25 @@ def get_specific_stat(player_stats, name):
               if data['label'] == name:
                   return data['value']
 
-
+# load the api key from the file named api_key
 trn_key = api_key_load("api_key")
 
-for player in ("akustic", "denahuen"):
+# Define the empty player list
+player_list = []
+
+# Get 2 players and add them to the player list. Don't except empty inputs.
+print("Enter the 2 players you would like to compare:" )
+while True:
+    new_player = input("> ")
+    if new_player is not "":
+        player_list.append(new_player)
+    else:
+        print("You didn't enter a player")
+    if len(player_list) == 2:
+        break
+
+# Get the stats we want for the players on the player list
+for player in player_list:
     p_stats = retrieve_stats(trn_key,player,"solo","agg")
     print("{}: ".format(player))
     for stat_label in ("K/D Ratio", "Rating", "Win %"):

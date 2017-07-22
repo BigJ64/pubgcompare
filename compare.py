ce8eb111625cd96bcaa8ca74f7caf8ff999c822e
Jordan Retterer <jordanr64@gmail.com>
2017-07-22 12:25:52 -0400

    Load API Key from file

--- compare.py
+++ compare.py
@@ -1,7 +1,27 @@
 from pypubg import core
+import re
 
-# Define our API key here
-trn_key = ""
+# Define our API key here from a file
+
+def api_key_load(file_name):
+    try:
+        file = open(file_name)
+    except IOError:
+        print("Error: {} file does not appear to exist.".format(file_name))
+        raise
+    else:
+        for line in file:
+            # Strip out any whitespace
+            li = line.strip()
+            # Set our matching pattern to try and verify the key before using it
+            pattern = re.compile("^\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$")
+            # Match our api key and set it as the key to use
+            if not li.startswith("#") and pattern.match(li):
+                key = li.rstrip()
+                return key
+                file.close()
+
+trn_key = api_key_load("api_key")
 
 # Setup the api connection
 pubgapi = core.PUBGAPI(trn_key)
